480a792daa298188679eea0d523fb73c55328ad1
HAitHAm HAmdy <hisambahaa@yahoo.com>
2015-11-11 11:23:16 +0400

    template

---
+++ admin/template/footer.php
@@ -0,0 +1,34 @@
+ 
+
+            </div>
+            <!-- /page content -->
+        </div>
+
+    </div>
+ <div id="custom_notifications" class="custom-notifications dsp_none">
+        <ul class="list-unstyled notifications clearfix" data-tabbed_notifications="notif-group">
+        </ul>
+        <div class="clearfix"></div>
+        <div id="notif-group" class="tabbed_notifications"></div>
+    </div>
+
+    <script src="<?php echo 'http://'.$_SERVER['SERVER_ADDR'].'/admin/template/';?>js/bootstrap.min.js"></script>
+
+    <!-- chart js -->
+    <script src="<?php echo 'http://'.$_SERVER['SERVER_ADDR'].'/admin/template/';?>js/chartjs/chart.min.js"></script>
+    <!-- bootstrap progress js -->
+    <script src="<?php echo 'http://'.$_SERVER['SERVER_ADDR'].'/admin/template/';?>js/progressbar/bootstrap-progressbar.min.js"></script>
+    <script src="<?php echo 'http://'.$_SERVER['SERVER_ADDR'].'/admin/template/';?>js/nicescroll/jquery.nicescroll.min.js"></script>
+    <!-- icheck -->
+    <script src="<?php echo 'http://'.$_SERVER['SERVER_ADDR'].'/admin/template/';?>js/icheck/icheck.min.js"></script>
+
+    <script src="<?php echo 'http://'.$_SERVER['SERVER_ADDR'].'/admin/template/';?>js/custom.js"></script>
+
+    <!-- moris js -->
+    <script src="<?php echo 'http://'.$_SERVER['SERVER_ADDR'].'/admin/template/';?>js/moris/raphael-min.js"></script>
+    <script src="<?php echo 'http://'.$_SERVER['SERVER_ADDR'].'/admin/template/';?>js/moris/morris.js"></script>
+    <script src="<?php echo 'http://'.$_SERVER['SERVER_ADDR'].'/admin/template/';?>js/moris/example.js"></script>
+
+</body>
+
+</html>
---
+++ admin/template/includes/sidebar.php
@@ -0,0 +1,114 @@
+<div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+
+                        <div class="menu_section">
+                            <h3>&nbsp;</h3>
+                            <ul class="nav side-menu">
+                                <li><a><i class="fa fa-home"></i> الرئيسية <span class="fa fa-chevron-down"></span></a>
+                                    <ul class="nav child_menu" style="display: none">
+                                        <li><a href="index.php">لوحة التحكم</a>
+                                        </li>
+                                        <li><a href="index2.php">لوحة التحكم 2</a>
+                                        </li>
+                                        <li><a href="index3.php">لوحة التحكم 3</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li><a><i class="fa fa-edit"></i> الفورمات <span class="fa fa-chevron-down"></span></a>
+                                    <ul class="nav child_menu" style="display: none">
+                                        <li><a href="form.php">فورمات عامة</a>
+                                        </li>
+                                        <li><a href="form_advanced.php">عناصر متقدمة</a>
+                                        </li>
+                                        <li><a href="form_validation.php">التحقق من الفورماتا</a>
+                                        </li>
+                                        <li><a href="form_wizards.php">معالج الفورم</a>
+                                        </li>
+                                        <li><a href="form_upload.php">فورم الرفع</a>
+                                        </li>
+                                        <li><a href="form_buttons.php">ازرار الفورم</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li><a><i class="fa fa-desktop"></i> عناصر الواجهة <span class="fa fa-chevron-down"></span></a>
+                                    <ul class="nav child_menu" style="display: none">
+                                        <li><a href="general_elements.php">عناصر عامة</a>
+                                        </li>
+                                        <li><a href="media_gallery.php">البوم الصور</a>
+                                        </li>
+                                        <li><a href="typography.php">الطباعة</a>
+                                        </li>
+                                        <li><a href="icons.php">الايقونات</a>
+                                        </li>
+                                        <li><a href="glyphicons.php">ايقونات Glyphicons</a>
+                                        </li>
+                                        <li><a href="widgets.php">الودجت</a>
+                                        </li>
+                                        <li><a href="invoice.php">الفاتورة</a>
+                                        </li>
+                                        <li><a href="inbox.php">صندوق الرسائل</a>
+                                        </li>
+                                        <li><a href="calender.php">التقويم</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li><a><i class="fa fa-table"></i> الجداول <span class="fa fa-chevron-down"></span></a>
+                                    <ul class="nav child_menu" style="display: none">
+                                        <li><a href="tables.php">الجداول</a>
+                                        </li>
+                                        <li><a href="tables_dynamic.php">جداول ديناميكية</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li><a><i class="fa fa-bar-chart-o"></i> عرض البيانات <span class="fa fa-chevron-down"></span></a>
+                                    <ul class="nav child_menu" style="display: none">
+                                        <li><a href="chartjs.php">Chart JS</a>
+                                        </li>
+                                        <li><a href="chartjs2.php">Chart JS2</a>
+                                        </li>
+                                        <li><a href="morisjs.php">Moris JS</a>
+                                        </li>
+                                        <li><a href="echarts.php">ECharts </a>
+                                        </li>
+                                        <li><a href="other_charts.php">أنواع أخرى </a>
+                                        </li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="menu_section">
+                            <h3></h3>
+                            <ul class="nav side-menu">
+                                <li><a><i class="fa fa-bug"></i> صفحات اضافية <span class="fa fa-chevron-down"></span></a>
+                                    <ul class="nav child_menu" style="display: none">
+                                        <li><a href="e_commerce.php">التجارة الالكترونية</a>
+                                        </li>
+                                        <li><a href="projects.php">المشاريع</a>
+                                        </li>
+                                        <li><a href="project_detail.php">تفاصيل المشروع</a>
+                                        </li>
+                                        <li><a href="contacts.php">جهات الاتصال</a>
+                                        </li>
+                                        <li><a href="profile.php">الصفحة الشخصية</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li><a><i class="fa fa-windows"></i> اكسترا <span class="fa fa-chevron-down"></span></a>
+                                    <ul class="nav child_menu" style="display: none">
+                                        <li><a href="page_404.php">صفحة 404</a>
+                                        </li>
+                                        <li><a href="page_500.php">عطب 500</a>
+                                        </li>
+                                        <li><a href="plain_page.php">صفحة فارغة</a>
+                                        </li>
+                                        <li><a href="login.php">صفحة الدخول</a>
+                                        </li>
+                                        <li><a href="pricing_tables.php">جداول الاسعار</a>
+                                        </li>
+
+                                    </ul>
+                                </li>
+                                
+                            </ul>
+                        </div>
+
+                    </div>
