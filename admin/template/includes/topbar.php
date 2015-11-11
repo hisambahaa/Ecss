c91177986448eeab17012e277720b0f5ef16f2c6
mouhsine bakhich <mouhsine.bakhich@gmail.com>
2015-11-11 12:57:20 +0400

    remove default messages from dropdown in topbar

--- admin/template/includes/topbar.php
+++ admin/template/includes/topbar.php
@@ -40,7 +40,7 @@
                                     <span class="badge bg-green">6</span>
                                 </a>
                                 <ul id="menu1" class="dropdown-menu list-unstyled msg_list animated fadeInDown" role="menu">
-                                    <li>
+                                  <!--   <li>
                                         <a>
                                             <span class="image">
                                         <img src="images/img.jpg" alt="Profile Image" />
@@ -53,53 +53,12 @@
                                         Film festivals used to be do-or-die moments for movie makers. They were where... 
                                     </span>
                                         </a>
-                                    </li>
-                                    <li>
-                                        <a>
-                                            <span class="image">
-                                        <img src="images/img.jpg" alt="Profile Image" />
-                                    </span>
-                                            <span>
-                                        <span>John Smith</span>
-                                            <span class="time">3 mins ago</span>
-                                            </span>
-                                            <span class="message">
-                                        Film festivals used to be do-or-die moments for movie makers. They were where... 
-                                    </span>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a>
-                                            <span class="image">
-                                        <img src="images/img.jpg" alt="Profile Image" />
-                                    </span>
-                                            <span>
-                                        <span>John Smith</span>
-                                            <span class="time">3 mins ago</span>
-                                            </span>
-                                            <span class="message">
-                                        Film festivals used to be do-or-die moments for movie makers. They were where... 
-                                    </span>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a>
-                                            <span class="image">
-                                        <img src="images/img.jpg" alt="Profile Image" />
-                                    </span>
-                                            <span>
-                                        <span>John Smith</span>
-                                            <span class="time">3 mins ago</span>
-                                            </span>
-                                            <span class="message">
-                                        Film festivals used to be do-or-die moments for movie makers. They were where... 
-                                    </span>
-                                        </a>
-                                    </li>
+                                    </li> -->
+                                   
                                     <li>
                                         <div class="text-center">
                                             <a>
-                                                <strong>See All Alerts</strong>
+                                                <strong>مشاهدة كل الرسائل</strong>
                                                 <i class="fa fa-angle-right"></i>
                                             </a>
                                         </div>
