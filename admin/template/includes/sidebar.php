2552e0570183e934a26bd3379ef2a6bb3952619e
mouhsine bakhich <mouhsine.bakhich@gmail.com>
2015-11-11 12:44:17 +0400

    add http base url to sidebar

--- admin/template/includes/sidebar.php
+++ admin/template/includes/sidebar.php
@@ -5,71 +5,71 @@
                             <ul class="nav side-menu">
                                 <li><a><i class="fa fa-home"></i> الرئيسية <span class="fa fa-chevron-down"></span></a>
                                     <ul class="nav child_menu" style="display: none">
-                                        <li><a href="index.php">لوحة التحكم</a>
+                                        <li><a href="<?php echo $config['http_base_url'] ?>admin/template/index.php">لوحة التحكم</a>
                                         </li>
-                                        <li><a href="index2.php">لوحة التحكم 2</a>
+                                        <li><a href="<?php echo $config['http_base_url'] ?>admin/template/index2.php">لوحة التحكم 2</a>
                                         </li>
-                                        <li><a href="index3.php">لوحة التحكم 3</a>
+                                        <li><a href="<?php echo $config['http_base_url'] ?>admin/template/index3.php">لوحة التحكم 3</a>
                                         </li>
                                     </ul>
                                 </li>
                                 <li><a><i class="fa fa-edit"></i> الفورمات <span class="fa fa-chevron-down"></span></a>
                                     <ul class="nav child_menu" style="display: none">
-                                        <li><a href="form.php">فورمات عامة</a>
+                                        <li><a href="<?php echo $config['http_base_url'] ?>admin/template/form.php">فورمات عامة</a>
                                         </li>
-                                        <li><a href="form_advanced.php">عناصر متقدمة</a>
+                                        <li><a href="<?php echo $config['http_base_url'] ?>admin/template/form_advanced.php">عناصر متقدمة</a>
                                         </li>
-                                        <li><a href="form_validation.php">التحقق من الفورماتا</a>
+                                        <li><a href="<?php echo $config['http_base_url'] ?>admin/template/form_validation.php">التحقق من الفورماتا</a>
                                         </li>
-                                        <li><a href="form_wizards.php">معالج الفورم</a>
+                                        <li><a href="<?php echo $config['http_base_url'] ?>admin/template/form_wizards.php">معالج الفورم</a>
                                         </li>
-                                        <li><a href="form_upload.php">فورم الرفع</a>
+                                        <li><a href="<?php echo $config['http_base_url'] ?>admin/template/form_upload.php">فورم الرفع</a>
                                         </li>
-                                        <li><a href="form_buttons.php">ازرار الفورم</a>
+                                        <li><a href="<?php echo $config['http_base_url'] ?>admin/template/form_buttons.php">ازرار الفورم</a>
                                         </li>
                                     </ul>
                                 </li>
                                 <li><a><i class="fa fa-desktop"></i> عناصر الواجهة <span class="fa fa-chevron-down"></span></a>
                                     <ul class="nav child_menu" style="display: none">
-                                        <li><a href="general_elements.php">عناصر عامة</a>
+                                        <li><a href="<?php echo $config['http_base_url'] ?>admin/template/general_elements.php">عناصر عامة</a>
                                         </li>
-                                        <li><a href="media_gallery.php">البوم الصور</a>
+                                        <li><a href="<?php echo $config['http_base_url'] ?>admin/template/media_gallery.php">البوم الصور</a>
                                         </li>
-                                        <li><a href="typography.php">الطباعة</a>
+                                        <li><a href="<?php echo $config['http_base_url'] ?>admin/template/typography.php">الطباعة</a>
                                         </li>
-                                        <li><a href="icons.php">الايقونات</a>
+                                        <li><a href="<?php echo $config['http_base_url'] ?>admin/template/icons.php">الايقونات</a>
                                         </li>
-                                        <li><a href="glyphicons.php">ايقونات Glyphicons</a>
+                                        <li><a href="<?php echo $config['http_base_url'] ?>admin/template/glyphicons.php">ايقونات Glyphicons</a>
                                         </li>
-                                        <li><a href="widgets.php">الودجت</a>
+                                        <li><a href="<?php echo $config['http_base_url'] ?>admin/template/widgets.php">الودجت</a>
                                         </li>
-                                        <li><a href="invoice.php">الفاتورة</a>
+                                        <li><a href="<?php echo $config['http_base_url'] ?>admin/template/invoice.php">الفاتورة</a>
                                         </li>
-                                        <li><a href="inbox.php">صندوق الرسائل</a>
+                                        <li><a href="<?php echo $config['http_base_url'] ?>admin/template/inbox.php">صندوق الرسائل</a>
                                         </li>
-                                        <li><a href="calender.php">التقويم</a>
+                                        <li><a href="<?php echo $config['http_base_url'] ?>admin/template/calender.php">التقويم</a>
                                         </li>
                                     </ul>
                                 </li>
                                 <li><a><i class="fa fa-table"></i> الجداول <span class="fa fa-chevron-down"></span></a>
                                     <ul class="nav child_menu" style="display: none">
-                                        <li><a href="tables.php">الجداول</a>
+                                        <li><a href="<?php echo $config['http_base_url'] ?>admin/template/tables.php">الجداول</a>
                                         </li>
-                                        <li><a href="tables_dynamic.php">جداول ديناميكية</a>
+                                        <li><a href="<?php echo $config['http_base_url'] ?>admin/template/tables_dynamic.php">جداول ديناميكية</a>
                                         </li>
                                     </ul>
                                 </li>
                                 <li><a><i class="fa fa-bar-chart-o"></i> عرض البيانات <span class="fa fa-chevron-down"></span></a>
                                     <ul class="nav child_menu" style="display: none">
-                                        <li><a href="chartjs.php">Chart JS</a>
+                                        <li><a href="<?php echo $config['http_base_url'] ?>admin/template/chartjs.php">Chart JS</a>
                                         </li>
-                                        <li><a href="chartjs2.php">Chart JS2</a>
+                                        <li><a href="<?php echo $config['http_base_url'] ?>admin/template/chartjs2.php">Chart JS2</a>
                                         </li>
-                                        <li><a href="morisjs.php">Moris JS</a>
+                                        <li><a href="<?php echo $config['http_base_url'] ?>admin/template/morisjs.php">Moris JS</a>
                                         </li>
-                                        <li><a href="echarts.php">ECharts </a>
+                                        <li><a href="<?php echo $config['http_base_url'] ?>admin/template/echarts.php">ECharts </a>
                                         </li>
-                                        <li><a href="other_charts.php">أنواع أخرى </a>
+                                        <li><a href="<?php echo $config['http_base_url'] ?>admin/template/other_charts.php">أنواع أخرى </a>
                                         </li>
                                     </ul>
                                 </li>
@@ -80,29 +80,29 @@
                             <ul class="nav side-menu">
                                 <li><a><i class="fa fa-bug"></i> صفحات اضافية <span class="fa fa-chevron-down"></span></a>
                                     <ul class="nav child_menu" style="display: none">
-                                        <li><a href="e_commerce.php">التجارة الالكترونية</a>
+                                        <li><a href="<?php echo $config['http_base_url'] ?>admin/template/e_commerce.php">التجارة الالكترونية</a>
                                         </li>
-                                        <li><a href="projects.php">المشاريع</a>
+                                        <li><a href="<?php echo $config['http_base_url'] ?>admin/template/projects.php">المشاريع</a>
                                         </li>
-                                        <li><a href="project_detail.php">تفاصيل المشروع</a>
+                                        <li><a href="<?php echo $config['http_base_url'] ?>admin/template/project_detail.php">تفاصيل المشروع</a>
                                         </li>
-                                        <li><a href="contacts.php">جهات الاتصال</a>
+                                        <li><a href="<?php echo $config['http_base_url'] ?>admin/template/contacts.php">جهات الاتصال</a>
                                         </li>
-                                        <li><a href="profile.php">الصفحة الشخصية</a>
+                                        <li><a href="<?php echo $config['http_base_url'] ?>admin/template/profile.php">الصفحة الشخصية</a>
                                         </li>
                                     </ul>
                                 </li>
                                 <li><a><i class="fa fa-windows"></i> اكسترا <span class="fa fa-chevron-down"></span></a>
                                     <ul class="nav child_menu" style="display: none">
-                                        <li><a href="page_404.php">صفحة 404</a>
+                                        <li><a href="<?php echo $config['http_base_url'] ?>admin/template/page_404.php">صفحة 404</a>
                                         </li>
-                                        <li><a href="page_500.php">عطب 500</a>
+                                        <li><a href="<?php echo $config['http_base_url'] ?>admin/template/page_500.php">عطب 500</a>
                                         </li>
-                                        <li><a href="plain_page.php">صفحة فارغة</a>
+                                        <li><a href="<?php echo $config['http_base_url'] ?>admin/template/plain_page.php">صفحة فارغة</a>
                                         </li>
-                                        <li><a href="login.php">صفحة الدخول</a>
+                                        <li><a href="<?php echo $config['http_base_url'] ?>admin/template/login.php">صفحة الدخول</a>
                                         </li>
-                                        <li><a href="pricing_tables.php">جداول الاسعار</a>
+                                        <li><a href="<?php echo $config['http_base_url'] ?>admin/template/pricing_tables.php">جداول الاسعار</a>
                                         </li>
 
                                     </ul>
