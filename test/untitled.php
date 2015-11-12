c5523d7aed6610d1b6bc363167c7d69eda4cbf8d
samialmamari <sami.almamari@css.edu.om>
2015-11-12 14:20:35 +0400

    hisam test

--- test/untitled.php
+++ test/untitled.php
@@ -1,84 +1,9 @@
-<?php require_once('../Connections/dares_conn.php'); ?>
-<?php
-if (!function_exists("GetSQLValueString")) {
-function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
-{
-  if (PHP_VERSION < 6) {
-    $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
-  }
-
-  $theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
-
-  switch ($theType) {
-    case "text":
-      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
-      break;    
-    case "long":
-    case "int":
-      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
-      break;
-    case "double":
-      $theValue = ($theValue != "") ? doubleval($theValue) : "NULL";
-      break;
-    case "date":
-      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
-      break;
-    case "defined":
-      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
-      break;
-  }
-  return $theValue;
-}
-}
-
-$editFormAction = $_SERVER['PHP_SELF'];
-if (isset($_SERVER['QUERY_STRING'])) {
-  $editFormAction .= "?" . htmlentities($_SERVER['QUERY_STRING']);
-}
-
-if ((isset($_POST["MM_update"])) && ($_POST["MM_update"] == "form1")) {
-  $updateSQL = sprintf("UPDATE sys_users SET user_last_login=%s WHERE user_id=%s",
-                       GetSQLValueString($_POST['user_last_login'], "date"),
-                       GetSQLValueString($_POST['user_id'], "int"));
-
-  mysql_select_db($database_dares_conn, $dares_conn);
-  $Result1 = mysql_query($updateSQL, $dares_conn) or die(mysql_error());
-}
-
-mysql_select_db($database_dares_conn, $dares_conn);
-$query_get_users = "SELECT * FROM sys_users";
-$get_users = mysql_query($query_get_users, $dares_conn) or die(mysql_error());
-$row_get_users = mysql_fetch_assoc($get_users);
-$totalRows_get_users = mysql_num_rows($get_users);
-?>
-<?php include('../Connections/composer.php'); ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Untitled Document</title>
-</head>
-
-<body>
-<?php echo date('Y-m-d'); ?>
-<form action="<?php echo $editFormAction; ?>" method="post" name="form1" id="form1">
-  <table align="center">
-    <tr valign="baseline">
-      <td nowrap="nowrap" align="right">User_last_login:</td>
-      <td><input type="text" name="user_last_login" value="<?php echo date('Y-m-d'); ?>" size="32" /></td>
-    </tr>
-    <tr valign="baseline">
-      <td nowrap="nowrap" align="right">&nbsp;</td>
-      <td><input type="submit" value="Update record" /></td>
-    </tr>
-  </table>
-  <input type="hidden" name="user_id" value="<?php echo $row_get_users['user_id']; ?>" />
-  <input type="hidden" name="MM_update" value="form1" />
-  <input type="hidden" name="user_id" value="<?php echo $row_get_users['user_id']; ?>" />
-</form>
-<p>&nbsp;</p>
-</body>
-</html>
-<?php
-mysql_free_result($get_users);
+<?php 
+echo __DIR__;
+echo "<br>";
+echo dirname(__FILE__);
+echo "<br>";
+var_dump($_SERVER);
+var_dump(pathinfo($_SERVER['SCRIPT_FILENAME']));
+echo getcwd();
 ?>
