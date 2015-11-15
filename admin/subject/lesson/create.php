170e537a68cd77461500a4cd86f04f57101c8633
HAitHAm HAmdy <hisambahaa@yahoo.com>
2015-11-15 12:41:25 +0400

    check flash

--- admin/subject/lesson/create.php
+++ admin/subject/lesson/create.php
@@ -1,4 +1,5 @@
-<?php require_once('../../../Connections/dares_conn.php'); ?>
+<?php require_once('../../../config/boot.php'); ?>
+use \McKay\Flash;
 <?php
 if (!function_exists("GetSQLValueString")) {
 function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
@@ -53,50 +54,89 @@ if ((isset($_POST["MM_insert"])) && ($_POST["MM_insert"] == "form1")) {
     $insertGoTo .= (strpos($insertGoTo, '?')) ? "&" : "?";
     $insertGoTo .= $_SERVER['QUERY_STRING'].'subid='.$_POST['lesson_sub_id'];
   }
+  Flash::success("تم اضافة الدرس بنجاح");
   header(sprintf("Location: %s", $insertGoTo));
 }
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Untitled Document</title>
-</head>
+<?php 
+// html page title
+$pageTitle='إضافة درس';
+// require page header
+require_once $config['base_url'].'/admin/template/includes/header.php';
+?>
+<!-- page content -->
 
-<body>
-<form action="<?php echo $editFormAction; ?>" method="post" name="form1" id="form1">
-  <table align="center">
-    <tr valign="baseline">
-      <td nowrap="nowrap" align="right">Lesson_name:</td>
-      <td><input type="text" name="lesson_name" value="" size="32" /></td>
-    </tr>
-    <tr valign="baseline">
-      <td nowrap="nowrap" align="right">Lesson_order:</td>
-      <td><input type="text" name="lesson_order" value="" size="32" /></td>
-    </tr>
-    <tr valign="baseline">
-      <td nowrap="nowrap" align="right">Lesson_type:</td>
-      <td><select name="lesson_type">
+    <div class="col-md-12 col-sm-12 col-xs-12">
+      <div class="x_panel" style="min-height:600px;">
+        <div class="x_title">
+          <h2>الدروس</h2>
+           <div class="clearfix"></div>
+         </div>
+         <div class="x_content">
+         
+<form action="<?php echo $editFormAction; ?>" method="post" name="form1" id="form1" class="form-horizontal form-label-left" data-parsley-validate>
+
+     <div class="form-group">
+          <label class="control-label col-md-3" for="faculty_name">
+          Lesson_name<?php //echo $ecss_lang['ACADEMY_NAME'] ?>
+          <span class="required">*</span>
+          </label>
+          <div class="col-md-7">
+              <input type="text" name='lesson_name' id="lesson_name" required="required" class="form-control col-md-7 col-xs-12" />
+          </div>
+      </div>
+      
+      <div class="form-group">
+          <label class="control-label col-md-3" for="faculty_name">
+          Lesson_order<?php //echo $ecss_lang['ACADEMY_NAME'] ?>
+          <span class="required">*</span>
+          </label>
+          <div class="col-md-7">
+              <input type="text" name='lesson_order' id="lesson_order" required="required" class="form-control col-md-7 col-xs-12" />
+          </div>
+      </div>
+      
+      <div class="form-group">
+          <label class="control-label col-md-3" for="faculty_name">
+          Lesson_type<?php //echo $ecss_lang['ACADEMY_NAME'] ?>
+          <span class="required">*</span>
+          </label>
+          <div class="col-md-7">
+              <select name="lesson_type">
         <option value="1" <?php if (!(strcmp(1, ""))) {echo "SELECTED";} ?>>درس</option>
         <option value="0" <?php if (!(strcmp(0, ""))) {echo "SELECTED";} ?>>مقدمة</option>
-      </select></td>
-    </tr>
-    <tr valign="baseline">
-      <td nowrap="nowrap" align="right">Lesson_state:</td>
-      <td><select name="lesson_state">
+      </select>
+          </div>
+      </div>
+      
+      <div class="form-group">
+          <label class="control-label col-md-3" for="faculty_name">
+          Lesson_state<?php //echo $ecss_lang['ACADEMY_NAME'] ?>
+          <span class="required">*</span>
+          </label>
+          <div class="col-md-7">
+          <select name="lesson_state">
         <option value="1" <?php if (!(strcmp(1, ""))) {echo "SELECTED";} ?>>نشط</option>
         <option value="0" <?php if (!(strcmp(0, ""))) {echo "SELECTED";} ?>>غير نشط</option>
-      </select></td>
-    </tr>
-    <tr valign="baseline">
-      <td nowrap="nowrap" align="right">&nbsp;</td>
-      <td><input type="submit" value="Insert record" /></td>
-    </tr>
-  </table>
-  <input type="hidden" name="lesson_sub_id" value="1" />
+      </select>
+          </div>
+      </div>
+
+  <div class="ln_solid"></div>
+   <div class="form-group">
+            <div class="col-md-9 col-sm-9 col-xs-12 col-md-offset-3">
+                <a href='index.php?subid=<?php echo $_GET['subid']; ?>' class="btn btn-default pull-left">
+                <i class="fa fa-close"></i> <?php echo $ecss_lang['CANCEL'] ?></a>
+                <button type="submit" class="btn btn-success pull-left">
+                <i class="fa fa-save"></i> <?php echo $ecss_lang['SUBMIT'] ?>
+                </button>
+            </div>
+        </div>
+        
+  <input type="hidden" name="lesson_sub_id" value="<?php echo $_GET['subid']; ?>" />
   <input type="hidden" name="lesson_created_by" value="1" />
   <input type="hidden" name="MM_insert" value="form1" />
 </form>
-<p>&nbsp;</p>
-</body>
-</html>
+<script src="<?php echo $config['http_base_url'] ?>admin/template/js/icheck/icheck.min.js"></script>
+
+<?php require_once $config['base_url'].'/admin/template/includes/footer.php'; ?>
